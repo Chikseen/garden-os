494ee93de1395a634c169202d6be3a6b04be12ef
Tim Menzel <tim.menzel@appsfactory.de>
2023-03-30 15:42:48 +0200

    added first animation for the FE

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,14 +3,21 @@
     <h1>This is a websocket test for now</h1>
     <div>
       <h3>MY LIVE DATA</h3>
-      <p>{{ testEvent }}</p>
+      <div style="width: 300px; height: 300px; resize: horizontal">
+        <FuelMeter :value="testEvent"></FuelMeter>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import FuelMeter from "@/components/FuelMeter.vue";
+
 export default {
   name: "App",
+  components: {
+    FuelMeter,
+  },
   props: {
     question: {
       type: Object,
@@ -29,7 +36,7 @@ export default {
   },
   created() {
     this.emitter.on("test", (e) => {
-      this.testEvent = e;
+      this.testEvent = (e.replace(",", ".") * 1).toFixed(1) * 1;
     });
   },
 };
---
+++ client/src/components/FuelMeter.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="fuelMeter_wrapper">
+    <span class="fuelMeter_wrapper_background" :style="`bottom: ${(value * 100) / 5}%`"></span>
+    <p class="fuelMeter_text">{{ value }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: { type: Number, default: 2.5 },
+  },
+};
+</script>
+
+<style lang="scss">
+.fuelMeter {
+  &_wrapper {
+    position: relative;
+    width: 300px;
+    padding-bottom: 125%;
+    background-color: rgba(0, 255, 255);
+    border-radius: 15px;
+    transform: perspective(500px) rotateX(-45deg) translateZ(-500px) translateY(225px);
+    overflow: hidden;
+
+    &_background {
+      position: absolute;
+      left: 0;
+      height: 200%;
+      width: 150%;
+      background: rgb(23, 173, 81);
+      z-index: -10;
+      transition: all 0.5s;
+    }
+  }
+
+  &_text {
+    margin: 0;
+    height: 100%;
+    font-size: 4rem;
+    vertical-align: middle;
+    text-align: center;
+    z-index: 10;
+  }
+}
+</style>
