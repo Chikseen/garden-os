dd72226f05af9f7c9ffbd0c345e79f252d3c5f44
Tim Menzel <tim_menzel@t-online.de>
2023-04-23 17:02:51 +0200

    Fixed client connection

--- client/src/hub.js
+++ client/src/hub.js
@@ -2,42 +2,54 @@ import { HubConnectionBuilder, LogLevel } from "@aspnet/signalr";
 
 export default {
   async install(Vue) {
+    // Try connection to remote
     try {
-      console.log("Check conection to Remote IP: " + process.env.VUE_APP_PI_HOST);
-      const connection = new HubConnectionBuilder().withUrl(`http://${process.env.VUE_APP_PI_HOST}:${process.env.VUE_APP_PI_PORT}/hub`).configureLogging(LogLevel.Information).build();
-
-      // get and set Init Values
-      const initData = await fetch(`http://${process.env.VUE_APP_PI_HOST}:${process.env.VUE_APP_PI_PORT}`).then((response) => response.json());
-      Vue.config.globalProperties.emitter.emit("Event", initData);
-
-      Vue.config.globalProperties.emitter.on("closeConnection", () => {
-        connection.stop;
-      });
-
-      connection.start();
-      connection.on("SendMyEvent", (payload) => {
-        Vue.config.globalProperties.emitter.emit("Event", payload);
-      });
-
-      Vue.provide("$hub", connection);
+      await this.CheckConnection(Vue, process.env.VUE_APP_PI_HOST);
     } catch (e) {
-      console.log("Check conection to Local IP: 192.168.8.100");
-      const connection = new HubConnectionBuilder().withUrl(`http://192.168.8.100:${process.env.VUE_APP_PI_PORT}/hub`).configureLogging(LogLevel.Information).build();
-
-      // get and set Init Values
-      const initData = await fetch(`http://192.168.8.100:${process.env.VUE_APP_PI_PORT}`).then((response) => response.json());
-      Vue.config.globalProperties.emitter.emit("Event", initData);
-
-      Vue.config.globalProperties.emitter.on("closeConnection", () => {
-        connection.stop;
-      });
-
-      connection.start();
-      connection.on("SendMyEvent", (payload) => {
-        Vue.config.globalProperties.emitter.emit("Event", payload);
-      });
-
-      Vue.provide("$hub", connection);
+      console.log(e);
+      // Try connection to last known Ip
+      try {
+        await this.CheckConnection(Vue, localStorage.getItem("lastWorkingSubIp"));
+      } catch (e) {
+        console.log(e);
+        // Try search new Ip
+        let hasConnection = false;
+        let subIp = 100;
+        while (!hasConnection) {
+          try {
+            const ip = `192.168.8.${subIp}`;
+            await this.CheckConnection(Vue, ip);
+            hasConnection = true;
+            localStorage.setItem("lastWorkingSubIp", subIp);
+          } catch (e) {
+            console.log(e);
+            subIp++;
+            if (subIp > 255) subIp = 100;
+            console.error(`Could not connect to any service`);
+          }
+        }
+      }
     }
+  },
+
+  async CheckConnection(Vue, ip) {
+    console.log(`${ip}:${process.env.VUE_APP_PI_PORT}`);
+    const connection = new HubConnectionBuilder().withUrl(`http://${ip}:${process.env.VUE_APP_PI_PORT}/hub`).configureLogging(LogLevel.Information).build();
+    console.log(connection);
+    // get and set Init Values
+    const initData = await fetch(`http://${ip}:${process.env.VUE_APP_PI_PORT}`).then((response) => response.json());
+    Vue.config.globalProperties.emitter.emit("Event", initData);
+
+    Vue.config.globalProperties.emitter.on("closeConnection", () => {
+      connection.stop;
+    });
+
+    connection.start();
+    connection.on("SendMyEvent", (payload) => {
+      Vue.config.globalProperties.emitter.emit("Event", payload);
+    });
+
+    Vue.provide("$hub", connection);
+    console.log("Connection succed");
   },
 };
