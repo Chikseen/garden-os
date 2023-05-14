922b8cfa3bf672293b2787f5748257b66a8e8d65
Tim Menzel <tim.menzel@appsfactory.de>
2023-05-14 12:39:43 +0200

    ups forgot untracked

---
+++ main-service/HardwareController/DeviceHandler.cs
@@ -0,0 +1,12 @@
+using System.Device.Gpio;
+using System.Device.I2c;
+using Iot.Device.CharacterLcd;
+using Iot.Device.Pcx857x;
+
+namespace MainService.Hardware
+{
+    public static class DeviceHandler
+    {
+        
+     }
+}
---
+++ main-service/Models/DeviceDataModel.cs
@@ -0,0 +1,20 @@
+using System.Text.Json.Serialization;
+
+public class DevicesData
+{
+    [JsonInclude]
+    public List<Device> Devices = new();
+
+    public DevicesData(List<Dictionary<String, String>> devicesList)
+    {
+        List<Device> devices = new();
+        foreach (var deciveDictionary in devicesList)
+        {
+            devices.Add(new Device(deciveDictionary));
+        }
+        Devices = devices;
+    }
+
+    public DevicesData()
+    { }
+}
