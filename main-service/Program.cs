71996563333be53f4a9cc0f66fdb7fb965134d8c
Tim Menzel <tim_menzel@t-online.de>
2023-06-22 09:51:42 +0200

    work on login and pwa improvments

--- main-service/Program.cs
+++ main-service/Program.cs
@@ -1,5 +1,7 @@
 using MainService.Hub;
 using MainService.DB;
+using System.Net;
+using System.Net.Sockets;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,13 +16,29 @@ builder.Services.AddCors();
 
 var app = builder.Build();
 
-app.UseCors(builder =>
-    builder
-        .WithOrigins("https://gardenos.drunc.net/", "https://gardenos.drunc.net", "http://localhost:8080")
-        .AllowAnyMethod()
-        .AllowAnyHeader()
-        .AllowCredentials()
-);
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors(builder =>
+        builder
+            .WithOrigins("http://localhost:8080", $"http://{GetLocalIPAddress()}:8080", $"https://{GetLocalIPAddress()}:8080")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials()
+    );
+    //app.Urls.Add($"https://{GetLocalIPAddress()}:5082");
+    app.Urls.Add($"https://localhost:5082");
+}
+else
+{
+    app.UseCors(builder =>
+        builder
+            .WithOrigins("https://gardenos.drunc.net/", "https://gardenos.drunc.net")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials()
+    );
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -28,7 +46,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
- 
+
 app.UseAuthorization();
 app.MapControllers();
 app.MapHub<MainHub>("/hub");
@@ -36,3 +54,16 @@ app.MapHub<MainHub>("/hub");
 MainDB.Init();
 
 app.Run();
+
+static string GetLocalIPAddress()
+{
+    var host = Dns.GetHostEntry(Dns.GetHostName());
+    foreach (var ip in host.AddressList)
+    {
+        if (ip.AddressFamily == AddressFamily.InterNetwork)
+        {
+            return ip.ToString();
+        }
+    }
+    throw new Exception("No network adapters with an IPv4 address in the system!");
+}
