5f9bdb4ea7eec35d75d588ec472c96c8f91f024a
Tim Menzel <tim_menzel@t-online.de>
2023-08-03 15:45:01 +0200

    Improved cool new icons

--- client/src/components/DynIcons/UVIndex.vue
+++ client/src/components/DynIcons/UVIndex.vue
@@ -1,15 +1,22 @@
 <template>
 	<div class="uvi-icon_wrapper">
+		<div class="uvi-icon">
+			<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="100%" height="100%">
+				<path :fill="style.color" d="M15 1A1 1 0 0115 29 1 1 0 0115 1M15 2A1 1 0 0015 28 1 1 0 0015 2" />
+			</svg>
+			<div class="uvi-icon_cover" :style="`bottom: ${value * 10}%;`"></div>
+		</div>
 		<svg class="uvi-icon" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="100%"
 			height="100%">
-			<path :fill="style.color" d="M15 1A1 1 0 0115 29 1 1 0 0115 1M15 2A1 1 0 0015 28 1 1 0 0015 2" />
 			<path :fill="style.color" d="M15 9A1 1 0 0015 21 1 1 0 0015 9" />
 		</svg>
-		<svg class="uvi-icon" v-for="i in style.sunBeam" :key="i"
-			:style="`transform: rotateZ(${360 / style.sunBeam * i}deg);`" version="1.1" xmlns="http://www.w3.org/2000/svg"
-			viewBox="0 0 30 30" width="100%" height="100%">
-			<path :fill="style.color" d="M14.5 4A1.5 1 0 0115.5 4L15.5 7A1.5 1 0 0114.5 7L14.5 4" />
-		</svg>
+		<div class="uvi-icon_spinner">
+			<svg class="uvi-icon" v-for="i in style.sunBeam" :key="i"
+				:style="`transform: rotateZ(${360 / style.sunBeam * i}deg);`" version="1.1"
+				xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="100%" height="100%">
+				<path :fill="style.color" d="M14.5 4A1.5 1 0 0115.5 4L15.5 7A1.5 1 0 0114.5 7L14.5 4" />
+			</svg>
+		</div>
 	</div>
 </template>
 
@@ -55,6 +62,7 @@ export default {
 	top: 0;
 	left: 0;
 	width: 100%;
+	height: 100%;
 	font-size: 2px;
 
 	&_wrapper {
@@ -69,6 +77,33 @@ export default {
 		border-radius: 10px;
 		gap: 2px;
 		overflow: hidden;
+	}
+
+	&_cover {
+		position: absolute;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgb(255, 255, 255);
+	}
+
+	&_spinner {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		animation: uviSpinner 20s infinite linear;
+	}
+}
+
+@keyframes uviSpinner {
+	from {
+		transform: rotateZ(0deg);
+	}
+
+	to {
+		transform: rotateZ(360deg);
 	}
 }
 
